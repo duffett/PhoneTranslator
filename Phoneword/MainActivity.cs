76efe31b28c89564ca0d0222fdaa7a6480db1adc
duffett <duffett@gmail.com>
2016-07-08 16:06:28 -0400

    basic functionality descirbed by tutorial at https://developer.xamarin.com/guides/android/getting_started/hello,android/hello,android_quickstart/

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace Phoneword
 {
-    [Activity(Label = "Phoneword", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         int count = 1;
@@ -19,13 +19,46 @@ namespace Phoneword
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberTex);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var callButton = FindViewById<Button>(Resource.Id.CallButton);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            callButton.Enabled = false;
+           
+            var translatedNmber = string.Empty;
+            translateButton.Click += (object sender, EventArgs e) =>
+             {
+                 translatedNmber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                 if (string.IsNullOrWhiteSpace(translatedNmber))
+                 {
+                     callButton.Text = "Call";
+                     callButton.Enabled = false;
+                 }
+                 else
+                 {
+                     callButton.Text = string.Format("Call {0}", translatedNmber);
+                     callButton.Enabled = true;
+                 }
+             };
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage(string.Format("Call {0}?", translatedNmber));
+                callDialog.SetNeutralButton("Call", delegate
+                 {
+                     var callIntent = new Intent(Intent.ActionCall);
+                     callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNmber));
+                     StartActivity(callIntent);
+                 });
+
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
         }
+        
     }
 }
 
